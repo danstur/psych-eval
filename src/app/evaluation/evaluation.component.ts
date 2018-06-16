e1062ee041d7d9ecbfa6ecb615d0906d1f4a89c5
Daniel Sturm <voodoon42@gmail.com>
2018-06-17 00:04:24 +0200

    Basic symptom picker working.

--- src/app/evaluation/evaluation.component.ts
+++ src/app/evaluation/evaluation.component.ts
@@ -1,32 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { IllnessService } from '../illnesses/illness.service';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
+import { MatAutocompleteSelectedEvent } from '@angular/material';
 
 @Component({
   templateUrl: './evaluation.component.html',
 })
 export class EvaluationComponent implements OnInit {
   symptoms: string[];
-  filteredSymptoms: Observable<string[]>;
 
-  myControl: FormControl = new FormControl();
+  @ViewChild('symptomsInput') symptomsInput: ElementRef;
+  unpickedSymptoms: string[];
+  filteredSymptoms: Observable<string[]>;
+  selectedSymptoms: string[] = [];
+  symptomsControl: FormControl = new FormControl();
 
   constructor(private illnessService: IllnessService) {
   }
 
   async ngOnInit() {
     this.symptoms = await this.illnessService.getSymptoms();
-    this.filteredSymptoms = this.myControl.valueChanges.pipe(
+    this.unpickedSymptoms = this.symptoms;
+    this.filteredSymptoms = this.symptomsControl.valueChanges.pipe(
       startWith(''),
       map(val => this.filter(val))
     );
   }
 
   filter(filterString: string): string[] {
-    const normalizedFilter = filterString.toLocaleLowerCase();
-    return this.symptoms.filter(symptom => symptom.toLowerCase().includes(normalizedFilter));
+    if (!filterString) {
+      return this.unpickedSymptoms;
+    }
+    const normalizedFilter = filterString.toLowerCase();
+    return this.unpickedSymptoms.filter(symptom => symptom.toLowerCase().includes(normalizedFilter));
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    const pickedSymptom = event.option.value;
+    this.selectedSymptoms.push(pickedSymptom);
+    this.unpickedSymptoms = this.unpickedSymptoms.filter(symptom => symptom !== pickedSymptom);
+    this.symptomsInput.nativeElement.value = '';
+    this.symptomsControl.setValue(null);
   }
 }
 
