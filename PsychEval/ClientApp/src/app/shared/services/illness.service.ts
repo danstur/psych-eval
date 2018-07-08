9b26bf2c1d97cd4d83630edd2990c09b02610af7
Daniel Sturm <voodoon42@gmail.com>
2018-07-08 22:44:04 +0200

    Create base HttpClient

--- PsychEval/ClientApp/src/app/shared/services/illness.service.ts
+++ PsychEval/ClientApp/src/app/shared/services/illness.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import * as _ from 'lodash';
+import { ConfiguredHttpClient } from './configuredHttpClient';
 
 const configUrl = 'assets/illnesses.json';
 
@@ -11,25 +12,17 @@ const configUrl = 'assets/illnesses.json';
 export class IllnessService {
   private illnesses: Promise<Illness[]>;
 
-  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl) {
+  constructor(private http: ConfiguredHttpClient) {
     this.refresh();
   }
 
   async getIllnesses(): Promise<Illness[]> {
-    try {
-      return await this.illnesses;
-    } catch (exception) {
-      alert(`Could not load data. Exception: ${exception}`);
-    }
+    return await this.illnesses;
   }
 
   async getSymptoms(): Promise<string[]> {
-    try {
-      const illnesses = await this.illnesses;
-      return _(illnesses).flatMap(il => il.symptoms).uniq().value();
-    } catch (exception) {
-      alert(`Could not load data. Exception: ${exception}`);
-    }
+    const illnesses = await this.illnesses;
+    return _(illnesses).flatMap(il => il.symptoms).uniq().value();
   }
 
   refresh(): Promise<any> {
