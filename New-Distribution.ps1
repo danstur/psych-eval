1db61322af8b2f2dbb1794144926379b0cd8b717
Daniel Sturm <voodoon42@gmail.com>
2018-07-10 22:30:04 +0200

    Update to allow multiple distributions.

--- New-Distribution.ps1
+++ New-Distribution.ps1
@@ -1,17 +1,24 @@
 param(
+    [Parameter(Mandatory=$true)]
+    [ValidateSet('win7-x86','linux-x64','osx-x64')]
+    [string]$Runtime,
     [Parameter(Mandatory=$false)]
-    [string]$OutputPath='release.zip'
+    [string]$OutputDirectory
 )
 
 $configuration = 'release'
-$runtime = 'win7-x86'
-
+if (!$OutputDirectory) {
+    $OutputDirectory = (Get-Location).Path
+}
 $csproj = "$PSScriptRoot\PsychEval\PsychEval.csproj"
-dotnet publish -c $configuration  -r $runtime "`"$csproj`"" | Write-Output
+dotnet publish -c $configuration -r $runtime "`"$csproj`"" | Write-Output
 if ($LASTEXITCODE -ne 0) {
     throw "Publishing code failed with error $LASTEXITCODE."
 }
+$binaryPath = "$PSScriptRoot\PsychEval\bin\$configuration\netcoreapp2.1\$Runtime\publish"
+$version = (Get-ChildItem "$binaryPath\PsychEval.dll").VersionInfo.ProductVersion
+$outputPath = "$OutputDirectory\Psych-Eval_${release}_${version}.zip"
 if (Test-Path $OutputPath) {
     Remove-Item $OutputPath -Force
 }
-Compress-Archive -Path "$PSScriptRoot\PsychEval\bin\$configuration\netcoreapp2.1\$runtime\publish\*" -DestinationPath $OutputPath -CompressionLevel 'Optimal'
+Compress-Archive -Path "$binaryPath\*" -DestinationPath $OutputPath -CompressionLevel 'Optimal'
